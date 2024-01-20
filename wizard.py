210fdfe45f1c125e23dabfc442088d760651723c
Hamadabosbet <73755871+Hamadabosbet@users.noreply.github.com>
2024-01-20 16:11:19 +0200

    I have improved the wizard.py file to enhance its testability.

--- wizard.py
+++ wizard.py
@@ -43,102 +43,92 @@ class Wizard:
 
     def prev_or_next(self, num_phase: int) -> Union[str, None]:
         while True:
-            move = input(f"You in Phase {num_phase} ,Type (1) for Next  / (2) for Prev  /(3) for exit: ")
+            move = input(f"You are in Phase {num_phase}, Type (1) for Next / (2) for Prev / (3) for exit: ")
             if move == "1":
-                if num_phase == 1:
-                    num_phase += 1
+                num_phase = self.handle_next_move(num_phase)
+                if num_phase > 3:
                     self.create_phase(num_phase)
-                if num_phase == 2:
-                    num_phase += 1
-                    self.create_phase(num_phase)
-
-                if num_phase == 3:
-                    self.create_phase(num_phase + 1)
                     self.details["is_finished"] = True
                     return 'done'
             elif move == "2":
-                if num_phase == 1:
-                    print('You in Phase 1, You cant prev')
-                    display.show_phase(num_phase, self.details)
-                    self.check_if_update(self.phases[num_phase - 1])
-                elif num_phase == 2:
-                    num_phase -= 1
-                    print('You in Phase 1')
-                    display.show_phase(num_phase, self.details)
-                    self.check_if_update(self.phases[num_phase - 1])
-                elif num_phase == 3:
-                    num_phase -= 1
-                    print('You in Phase 2')
-                    display.show_phase(num_phase, self.details)
-                    self.check_if_update(self.phases[num_phase - 1])
-                elif num_phase == 4:
-                    print('You in Phase 3')
-                    display.show_phase(num_phase - 1, self.details)
-                    self.check_if_update(self.phases[num_phase - 2])
-
+                num_phase = self.handle_prev_move(num_phase)
             elif move == "3":
                 self.details["is_finished"] = False
                 return "done"
             else:
-                print("Invalid choice. Please enter '1' to continue Next or '2' to Prev.")
+                print("Invalid choice. Please enter '1' to continue Next, '2' to Prev, or '3' to exit.")
+
+    def handle_next_move(self, num_phase: int) -> int:
+        num_phase += 1
+        self.create_phase(num_phase)
+        return num_phase
+
+    def handle_prev_move(self, num_phase: int) -> int:
+        if num_phase == 1:
+            print('You are in Phase 1, You cannot go back.')
+        else:
+            num_phase -= 1
+            display.show_phase(num_phase, self.details)
+            self.check_if_update(self.phases[num_phase - 1])
+        return num_phase
 
     def start_wizard(self) -> None:
         my_connector.connect_to_db()
         print("Welcome to the Wizard!")
-        while True:
-            choice = input("Menu: 1) Start New 2) Continue: 3) History 4) Statistics 5) exit")
-            if choice == "1":
-                self.create_phase(1)
-                if_done = self.prev_or_next(1)
-                if if_done == 'done':
-                    my_connector.save_wizard_to_db(self.details)
-                    display.display_summary(self.details)
-                    self.phases = []
-                    self.wizards.append(self.details)
-                    self.details = {}
-
-            elif choice == "2":
-                my_connector.show_unfinished_wizards_from_db()
-                wizards = my_connector.load_wizards_from_db(is_finished=False)
-                if wizards:
-                    while True:
-                        chosen_name = input("Enter  the  name of wizard that  you want to continue registration for him: ")
-                        if chosen_name:
-                            chosen_wizard = next((wizard for wizard in wizards if wizard["Name"] == chosen_name),
-                                                 None)
-                            if chosen_wizard:
-                                self.details = chosen_wizard
-                                phase_number = int(input("Enter phase number: "))
-                                if phase_number > len(self.phases):
-                                    self.create_phase(phase_number)
-                                    if_done = self.prev_or_next(phase_number)
-                                    if if_done == 'done':
-                                        my_connector.save_wizard_to_db(self.details)
-                                        display.display_summary(self.details)
-                                        self.phases = []
-                                        self.wizards.append(self.details)
-                                        self.details = {}
-                                        break
-                                else:
-                                    self.show_phase(phase_number, self.details)
-                            else:
-                                print(f"No wizard found with the name: {chosen_name}. Try again.")
-                        else:
-                            print("Invalid input. Please enter your full name.")
+        try:
+            while True:
+                choice = input("Menu: 1) Start New 2) Continue: 3) History 4) Statistics 5) exit")
+                if choice == "1":
+                    self.start_new_wizard()
+                elif choice == "2":
+                    self.continue_wizard()
+                elif choice == "3":
+                    my_connector.show_wizards_history_from_db()
+                elif choice == "4":
+                    my_connector.show_statistics()
+                elif choice == "5":
+                    break
                 else:
-                    print("No unfinished wizards found.")
-                    return None
+                    print("Invalid choice!")
+        finally:
+              my_connector.close_db_connection()
 
-            elif choice == "3":
-                my_connector.show_wizards_history_from_db()
-            elif choice == "4":
-                my_connector.show_statistics()
-            elif choice == "5":
-                break
+    def start_new_wizard(self):
+        self.create_phase(1)
+        if_done = self.prev_or_next(1)
+        if if_done == 'done':
+            self.save_wizard_and_display_summary()
+
+    def continue_wizard(self):
+        my_connector.show_unfinished_wizards_from_db()
+        wizards = my_connector.load_wizards_from_db(is_finished=False)
+        if wizards:
+            chosen_name = input("Enter the name of the wizard you want to continue registration for: ")
+            chosen_wizard = next((wizard for wizard in wizards if wizard["Name"] == chosen_name), None)
+            if chosen_wizard:
+                self.details = chosen_wizard
+                self.process_continue_wizard()
             else:
-                print("Invalid choice!")
+                print(f"No wizard found with the name: {chosen_name}. Try again.")
+        else:
+            print("No unfinished wizards found.")
 
-        my_connector.close_db_connection()
+    def process_continue_wizard(self):
+        phase_number = int(input("Enter phase number: "))
+        if phase_number > len(self.phases):
+            self.create_phase(phase_number)
+            if_done = self.prev_or_next(phase_number)
+            if if_done == 'done':
+                self.save_wizard_and_display_summary()
+        else:
+            self.show_phase(phase_number, self.details)
+
+    def save_wizard_and_display_summary(self):
+        my_connector.save_wizard_to_db(self.details)
+        display.display_summary(self.details)
+        self.phases = []
+        self.wizards.append(self.details)
+        self.details = {}
 
 
 
